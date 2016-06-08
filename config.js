cc9ecee53f1abf320adac2fa1f95313c15860eaf
metherton <martinetherton63@gmail.com>
2016-06-08 21:56:01 +0200

    change db name

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'mongoUrl' : 'mongodb://localhost:27017/scrumtool',
     'facebook': {
         clientID: '1580329512259259',
         clientSecret: '3ba4033b8c2bbf30ea2b18ac64c38b2e',
